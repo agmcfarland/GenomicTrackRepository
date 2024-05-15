ca79e76194044e80e71fce4f84690d8be2b2738c
Alexander McFarland <alex.925@gmail.com>
2024-05-15 13:56:25 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Store scripts used to make Bushman Lab genomic tracks',
+    author='Alexander McFarland',
+    license='MIT',
+)
